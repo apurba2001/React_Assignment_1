dd789498e06c7782081e597a03e44a5d2f8bc740
apurba2001 <apurbaruidas0358@gmail.com>
2023-02-09 01:16:32 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const crypto = require("crypto");
+const crypto_orig_createHash = crypto.createHash;
+crypto.createHash = algorithm => crypto_orig_createHash(algorithm == "md4" ? "sha256" : algorithm);
+const path = require("path");
+const HtmlWebpackPlugin= require('html-webpack-plugin');
+module.exports = {
+    entry: './src/index.js',
+
+    output: {
+        path: path.join(__dirname,"/dist"),
+        filename: "index_bundle.js",
+    },
+    module:{
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader',
+                    },
+                    {
+                        loader: 'css-loader',
+                    },
+                ]
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    }
+};
